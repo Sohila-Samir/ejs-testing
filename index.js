14d17fd1c6a25afc615552635fa5c219e0b2c434
sohila samir <sohila.samir1911@gmail.com>
2022-01-24 14:30:16 +0200

    first ejs testing

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 2021;
+const serverJsonData = require('./data.json');
+
+//required to make the css style sheets work on ejs files
+app.use(express.static(path.join(__dirname, '/puplic')))
+
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+app.listen(port, () => {
+    console.log(`lestining on port: ${port}`);
+})
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/wlc/:name', (req, res) => {
+    //destructring the req param from the url
+    let {name} = req.params;
+    res.render('testing', { name });
+})
+
+app.get('/cond/:name', (req, res) => {
+    let {name} = req.params;
+    res.render('conditionals', { name });
+})
+
+app.get('/looping', (req, res) => {
+    let arr1 = ["L. for ma W", "S. for ma lil s", "L. for ma lil d"];
+    res.render('loops', { names : arr1 });
+})
+
+app.get('/r/:subraddit', (req, res) => {
+    let { subraddit } = req.params;
+    let radditData =  serverJsonData[subraddit];
+    console.log( radditData );
+    res.render('subraddits', { ...radditData })
+})
